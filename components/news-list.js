faaa51731eb7475405943d17d62da287606a74dc
KirillGorinDataArt <chetanvse@gmail.com>
2025-07-12 23:40:02 +0400

    Route groups

--- components/news-list.js
+++ components/news-list.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
 
-export default function NewsList({news}) {
-    return (<ul className="news-list">
-        {news.map(n => <li key={n.id}>
-            <Link href={`/news/${n.slug}`}>
-                <img src={`/images/news/${n.image}`} alt={n.title} />
-                <span>{n.title}</span>
-            </Link>
-        </li>)}
-    </ul>)
+export default function NewsList({ news }) {
+  return (
+    <ul className="news-list">
+      {news.map((n) => (
+        <li key={n.id}>
+          <Link href={`/app/(content)/news/${n.slug}`}>
+            <img src={`/images/news/${n.image}`} alt={n.title} />
+            <span>{n.title}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
